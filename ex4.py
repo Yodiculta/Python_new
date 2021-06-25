7b4d402886d9ad0f469d62506a71f0b5fdef1125
Daria <69566786+Yodiculta@users.noreply.github.com>
2021-06-25 20:31:20 +0300

    Update ex4.py
    интересно, почему maxi после функции так и остается указателем на 0 ((

--- ex4.py
+++ ex4.py
@@ -2,8 +2,23 @@
 # Для решения используйте цикл while и арифметические операции.
 d = input()
 
-max = 0
-for i in d:
-    if int(i) > max:
-        max = int(i)
-print(max)
+# max = 0
+# for i in d:
+#     if int(i) > max:
+#         max = int(i)
+# print(max)
+
+maxi=0
+def maxCount(num, maxi, t_len):
+    t_len -= 1
+    while int(t_len) >= 0:
+        next = num % 10
+        if next > maxi:
+            maxCount(num // 10, next, t_len)
+        elif maxi >= next:
+            maxCount(num // 10, maxi, t_len)
+        return 1
+    print(maxi)
+
+    
+maxCount(int(d), maxi, int(len(d)))
