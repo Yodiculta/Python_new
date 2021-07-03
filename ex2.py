517fd32bbbc61b5917190c3ee11977108b54f592
Daria <69566786+Yodiculta@users.noreply.github.com>
2021-07-03 22:31:42 +0300

    3_lesson

--- ex2.py
+++ ex2.py
@@ -1,8 +1,21 @@
-# 2. Пользователь вводит время в секундах.
-# Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
-d = int(input('введите время в секундах: '))
-mins = d // 60
-sec = d % 60
-hour = mins // 60
-mins = mins % 60
-print('{0}:{1}:{2}'.format(hour, mins, sec))
+# 2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
+# имя, фамилия, год рождения, город проживания, email, телефон.
+# Функция должна принимать параметры как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.
+
+def input_param():
+    name = input('input name: ')
+    s_name = input('input second name: ')
+    city = input('input city: ')
+    email = input('input email: ')
+    phone = input('input phone: ')
+    return [name, s_name, city, email, phone]
+
+
+def output_param(name, s_name, city, email, phone):
+    print(f"name is {name}, second name is {s_name}, live in {city}, email:{email}, phone:{phone}")
+
+
+data=input_param()
+output_param(name=data[0], s_name=data[1],
+             city=data[2], email=data[3], phone=data[4]
+             )
